62103595f1ae2f07bdbd8d6e202acad7d131adab
Camila Rose Cordero <camilarosecordero28@gmail.com>
2025-03-17 14:31:24 +0800

    Update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,6 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
+
         window.setTitle("Lost Thicket");
 
         GamePanel gamePanel = new GamePanel();
